48ea063a223e344715b6fe0407c0fc2928286b37
Soumabha <soumabha.sunny1987@gmail.com>
2025-05-28 20:50:58 +0530

    add tailwind and home page and added basic aurgument error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,17 @@
 class HomeController < ApplicationController
     def add_numbers
-        input_numbers = params[:numbers]
-        return 0 if input_numbers.nil? || input_numbers.empty?
-
-    sum = input_numbers.split(',').map(&:to_i).sum 
-    end
+    input_numbers = params[:numbers]
+    @result =
+      if input_numbers.present?
+        begin
+          numbers = input_numbers.split(',').map(&:to_i)
+          numbers.sum
+        rescue ArgumentError
+          "Please add comma-separated numbers."
+        end
+      else
+        0
+      end
+  end
+end
+    
